d6289e34112805a314acee2252f4f6d4095663cf
Mohamed Hamdy <moh04591@gmail.com>
2026-02-01 14:11:15 +0200

    Add supabase and react-query

--- features/cabins/CabinRow.jsx
+++ features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +39,22 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  console.log(Cabin);
+  console.log(Price);
+  console.log(Discount);
+  const { name, maxCapacity, reqularPrice, discount, image } = cabin;
+  return (
+    <TableRow role="row">
+      {<Img src={image} />}
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(reqularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;

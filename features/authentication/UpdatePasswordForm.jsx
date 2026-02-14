281a16f7ae5c57e28a4d9ccbb7c096f5619eea20
Mohamed Hamdy <moh04591@gmail.com>
2026-02-14 02:11:13 +0200

    Add account page

--- features/authentication/UpdatePasswordForm.jsx
+++ features/authentication/UpdatePasswordForm.jsx
@@ -19,7 +19,7 @@ function UpdatePasswordForm() {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow
-        label="Password (min 8 characters)"
+        label="New password (min 6 chars)"
         error={errors?.password?.message}
       >
         <Input
@@ -30,8 +30,8 @@ function UpdatePasswordForm() {
           {...register("password", {
             required: "This field is required",
             minLength: {
-              value: 8,
-              message: "Password needs a minimum of 8 characters",
+              value: 6,
+              message: "Password needs a minimum of 6 characters",
             },
           })}
         />

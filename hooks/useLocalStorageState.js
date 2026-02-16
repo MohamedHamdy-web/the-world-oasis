c7aec5a8409267b56d0e860ec550bfe5fe08b451
Mohamed Hamdy <moh04591@gmail.com>
2026-02-16 06:10:11 +0200

    fix

--- hooks/useLocalStorageState.js
+++ hooks/useLocalStorageState.js
@@ -3,14 +3,16 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialState;
+    return storedValue && storedValue !== "undefined"
+      ? JSON.parse(storedValue)
+      : initialState;
   });
 
   useEffect(
     function () {
       localStorage.setItem(key, JSON.stringify(value));
     },
-    [value, key]
+    [value, key],
   );
 
   return [value, setValue];

4de6d7e31c82fcf8e4069288fda37aa1ed2840b6
Mohamed Hamdy <moh04591@gmail.com>
2026-02-16 07:42:50 +0200

    fix

--- features/cabins/CabinRow.jsx
+++ features/cabins/CabinRow.jsx
@@ -90,7 +90,10 @@ function CabinRow({ cabin }) {
           <Menus.Menu>
             <Menus.Toggle id={cabinId} />
             <Menus.List id={cabinId}>
-              <Menus.Button onClick={handleDuplicate} icon={<HiSquare2Stack />}>
+              <Menus.Button
+                onClick={handleDuplicate}
+                icon={<HiSquare2Stack disabled={isCreating} />}
+              >
                 Duplicate
               </Menus.Button>
 
